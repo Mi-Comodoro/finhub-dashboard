eb7c442693543fbf231706179f2d8fad06de1294
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2026-05-26 08:46:12 -0500

    test deploy 3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,19 @@ module.exports = {
   apps: [
     {
       name: 'micomodoro',
+
       script: '.output/server/index.mjs',
+
       cwd: '/var/www/intranet/micomodoro-app',
 
       instances: 1,
+
       exec_mode: 'fork',
 
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
-        HOST: '0.0.0.0'
+        HOST: '0.0.0.0',
+        PORT: 3000
       }
     }
   ]
